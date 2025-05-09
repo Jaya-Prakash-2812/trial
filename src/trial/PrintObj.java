95c880552fce38ac84d512d46cc6c664b3e63647
Jaya-Prakash-2812 <jpofficial2812@gmail.com>
2025-05-09 18:29:18 +0530

    first test commit

---
+++ src/trial/PrintObj.java
@@ -0,0 +1,15 @@
+package trial;
+
+public class PrintObj {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			PrintObj obj = new PrintObj();
+			System.out.println(obj);
+		}
+
+	}
+
+
