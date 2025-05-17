e49ec6d3756c26889594fa4e3f16828e6ea0a117
Jaya-Prakash-2812 <jpofficial2812@gmail.com>
2025-05-17 17:38:30 +0530

    output inclusion

--- src/trial/PrintObj.java
+++ src/trial/PrintObj.java
@@ -4,11 +4,11 @@ public class PrintObj {
 	
 
 		public static void main(String[] args) {
-			// TODO Auto-generated method stub
 			
 			PrintObj obj = new PrintObj();
 			System.out.println(obj);
-			// o/p : package.classname @ hashCode
+			
+			// output : package.classname @ hashCode
 		}
 
 	}
